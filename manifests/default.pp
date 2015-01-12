2fa8e047df6b29dacc61515034073800f1d417d4
Simon Josi <me@yokto.net>
2015-01-12 10:38:32 +0100

    fix node definition

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,7 @@
 notify{"We are up and running!":}
 node 'puppet-dev.local' {
-  db_root_password => 'yachtokibNie',
-  db_app_password => 'whaiwimbyrie',
+  class { '::role_wordpress':
+    db_root_password => 'yachtokibNie',
+    db_app_password => 'whaiwimbyrie',
+  }
 }
